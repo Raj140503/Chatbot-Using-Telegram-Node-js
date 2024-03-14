439256e124e89f7187f3667c95e8482f683a04da
Raj Nanasaheb Patil <146241427+Raj140503@users.noreply.github.com>
2024-03-14 19:35:20 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-const botToken = '6353303960:AAEerHyPMNAsa18ES0bBzH_NbQUXytvwQsQ';
+const botToken = '.................................'; // add your own botToken here.
 const bot = new TelegramBot(botToken, { polling: true });
 function generateResponse(message) {
     return `You said: ${message}`;
